de41e51f8795007e47dad6d88552e53b0f5e38df
yuseohee27 <59616102+yuseohee27@users.noreply.github.com>
2020-04-09 07:41:55 +0900

    2324324

--- gr2_ysh/js/sh_user_w_semiDetail.js
+++ gr2_ysh/js/sh_user_w_semiDetail.js
@@ -6,9 +6,9 @@ var overTwo = document.querySelector('input[name=overTwo]');
 
 var countTable =  document.querySelector('.choice-num__count');
 
-if(overTwo.checked==true){
-    countTable.style.display = 'none';
-}
+// if(overTwo.checked==true){
+//     countTable.style.display = 'none';
+// }
 
 var askExit = document.querySelector('.exit__ask');
 var reviewExit = document.querySelector('.exit__review');
@@ -31,23 +31,38 @@ reviewExit.addEventListener('click',function(event){
     review.style.display = 'none';
 })
 
+// 인원 숫자 증감 부분
 var minus =  document.querySelector('.choice-num__count--minus');
-var number = document.querySelector('.choice-num__count--number');
+var number = document.querySelector('.number');
 var plus = document.querySelector('.choice-num__count--plus');
 
-var first = number.innerHTML;
+number.value=1;
+
+// -,+ 버튼 클릭시 숫자 증감
 plus.addEventListener('click',function(event){
-    if(Number(first)+1<=50){
-        number.innerHTML = Number(first)+1;
-        first = number.innerHTML;
+    if(Number(number.value)+1<=50){
+        number.value = Number(number.value)+1;
+
     }
 })
 minus.addEventListener('click',function(event){
-    if(Number(first)-1>0){
-        number.innerHTML = Number(first)-1;
-        first = number.innerHTML;
+    if(Number(number.value)-1>0){
+        number.value = Number(number.value)-1;
     }
 })
+
+//0미만 50초과시 숫자 자동 변경
+number.addEventListener('change',function(event){
+    if(Number(number.value)<=0){
+        number.value=1;
+
+    }else if(Number(number.value)>50){
+        number.value=50;
+    }
+})
+
+
+
 
 
 
