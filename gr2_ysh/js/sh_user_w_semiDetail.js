c5bb11ace6e785dce05919725b3587ad7d04d3f9
yuseohee27 <59616102+yuseohee27@users.noreply.github.com>
2020-04-08 18:15:09 +0900

    213213

--- gr2_ysh/js/sh_user_w_semiDetail.js
+++ gr2_ysh/js/sh_user_w_semiDetail.js
@@ -1,19 +1,13 @@
 var radio1 = document.querySelector('#radio1');
 var radio2 = document.querySelector('#radio2');
 
-var clicked1 = document.querySelector('.clicked1');
-var clicked2 = document.querySelector('.clicked2');
-var nonclick1 = document.querySelector('.nonclick1');
-var nonclick2 = document.querySelector('.nonclick2');
+var one = document.querySelector('[name=one]');
+var overTwo = document.querySelector('input[name=overTwo]');
 
 var countTable =  document.querySelector('.choice-num__count');
 
-if(radio1.checked==true){
-    clicked1.style.visibility='visible';
-    nonclick1.style.visibility='hidden';
-}else{
-    clicked1.style.visibility='hidden';
-    nonclick1.style.visibility='visible';
+if(overTwo.checked==true){
+    countTable.style.display = 'none';
 }
 
 var askExit = document.querySelector('.exit__ask');
