8ff30313adb9598d7fdfd7a4a3e055898e9c9622
yuseohee27 <59616102+yuseohee27@users.noreply.github.com>
2020-04-08 17:30:08 +0900

    mmlmkl

--- gr2_ysh/js/sh_user_w_semiDetail.js
+++ gr2_ysh/js/sh_user_w_semiDetail.js
@@ -13,3 +13,24 @@ if(radio1.checked==true){
     clicked1.style.visibility='hidden';
     nonclick1.style.visibility='visible';
 }
+
+var askExit = document.querySelector('.exit__ask');
+var reviewExit = document.querySelector('.exit__review');
+var ask = document.querySelector('.ask');
+var review = document.querySelector('.review');
+var askButton = document.querySelector('.ask--button');
+var reviewButton = document.querySelector('.review--button');
+
+askButton.addEventListener('click',function(event){
+    ask.style.display = 'block';
+})
+askExit.addEventListener('click',function(event){
+    ask.style.display = 'none';
+})
+
+reviewButton.addEventListener('click',function(event){
+    review.style.display = 'block';
+})
+reviewExit.addEventListener('click',function(event){
+    review.style.display = 'none';
+})
