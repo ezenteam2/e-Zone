b259b72238525acc47f2af785ec617b3c344708a
yuseohee27 <59616102+yuseohee27@users.noreply.github.com>
2020-04-13 18:20:32 +0900

    hjghj

--- gr2_ysh/js/sh_user_w_semiPay.js
+++ gr2_ysh/js/sh_user_w_semiPay.js
@@ -2,19 +2,19 @@
 var block = $('.block').get();
 var none = $('.none').get();
 var first = $('#first').get();
-for(var i = 0; i<block.length; i++){
-    block[i].css('display','none');
-    none[i].css('display','block');
+for(let i = 0; i<block.length; i++){
+    $(block[i]).css('display','none');
+    $(none[i]).css('display','block');
     console.log(block[i])
-    none[i].click(function(e){
-        none[i].css('display','none');
-        block[i].css('display','block');
-        first[i].trigger('click');
+    $(none[i]).click(function(e){
+        $(none[i]).css('display','none');
+        $(block[i]).css('display','block');
+        $(first[i]).trigger('click');
     })
-    block[i].click(function(e){
-        block[i].css('display','none');
-        none[i].css('display','block');
-        first[i].trigger('click');
+    $(block[i]).click(function(e){
+        $(block[i]).css('display','none');
+        $(none[i]).css('display','block');
+        $(first[i]).trigger('click');
     })
 }
 
