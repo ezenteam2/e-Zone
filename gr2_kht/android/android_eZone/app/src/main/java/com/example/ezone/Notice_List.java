b67ee65511265253898b4f09e412c1d685680ed4
esseone7 <59997115+esseone7@users.noreply.github.com>
2020-04-27 23:26:30 +0900

    asdf

---
+++ gr2_kht/android/android_eZone/app/src/main/java/com/example/ezone/Notice_List.java
@@ -0,0 +1,14 @@
+package com.example.ezone;
+
+public class Notice_List {
+
+    private Notice_Result notice_result;
+
+    public Notice_Result getNotice_result() {
+        return notice_result;
+    }
+
+    public void setNotice_result(Notice_Result notice_result) {
+        this.notice_result = notice_result;
+    }
+}
